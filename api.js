d2991b96df6536b4bd056c4b79cef4c09a01e1c1
rodri-oyonarte <oyonarte.90@gmail.com>
2015-09-09 10:29:54 -0300

    datos
    change global var datos to each method

--- api.js
+++ api.js
@@ -8,29 +8,34 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 
-var datos={}
+
 var server = app.listen(3000, function() {
+	var datos={};
     console.log('Escuchando en el puerto %d', server.address().port);
 });
 
 app.get('/calendar/:calendarId/event', function (req1, resp1){
+	var datos={};
 	datos.calendarId = req1.params.calendarId;
   	google.start('listEvents',resp1,datos);
 });
 
 app.get('/calendar/:calendarId/event/:eventId', function (req1, resp1){
+	var datos={};
 	datos.calendarId = req1.params.calendarId;
 	datos.eventId = req1.params.eventId;
     google.start('getEvent',resp1,datos);
 });
 
 app.delete('/calendar/:calendarId/event/:eventId', function (req1, resp1){
+	var datos={};
 	datos.calendarId = req1.params.calendarId;
 	datos.eventId = req1.params.eventId;
     google.start('deleteEvent',resp1,datos);
 });
 
 app.put('/calendar/:calendarId/event/:eventId', function (req1, resp1){
+	var datos={};
 	datos.calendarId = req1.params.calendarId;
 	datos.eventId = req1.params.eventId;
 	datos.eventUpdate = req1.body;
@@ -38,6 +43,7 @@ app.put('/calendar/:calendarId/event/:eventId', function (req1, resp1){
 });
 
 app.post('/calendar/:calendarId/event', function (req, res){
+	var datos={};
 	var today = new Date();
 	datos.calendarId = req.params.calendarId;
 	req.body.summary = req.body.summary + " creado por APIsistDist"
